7d5b579a05675a8cfcfeb68a9f1dc9530f18682e
Cho <zhushenzhe0531@gmail.com>
2024-09-29 03:44:03 +0800

    UPDATED

--- src/steer_experiments/sae_trait_run/TRAIT_Dark/sae_steer_coef_change_gemma_2b_it.sh
+++ src/steer_experiments/sae_trait_run/TRAIT_Dark/sae_steer_coef_change_gemma_2b_it.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-python SAE/sae_run.py \
-    --model_name gemma-2b-it \
-    --sae_name gemma-2b-it-res-jb \
-    --sae_id blocks.12.hook_resid_post \
-    --tokenizer_name google/gemma-2b-it \
-    --layer 12 \
-    --coeff 200 \
-    --bg_type fixed \
-    --steer_mode \
-    --steer_file_path ../data/SAE/bg_features/coeff/gemma-2b-it.json \
-    --prompt_type 1 \
-    --inference_type chat \
-    --save_dir_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff200 \
-    --testset TRAIT_Dark
+#python SAE/sae_run.py \
+#    --model_name gemma-2b-it \
+#    --sae_name gemma-2b-it-res-jb \
+#    --sae_id blocks.12.hook_resid_post \
+#    --tokenizer_name google/gemma-2b-it \
+#    --layer 12 \
+#    --coeff 200 \
+#    --bg_type fixed \
+#    --steer_mode \
+#    --steer_file_path ../data/SAE/bg_features/coeff/gemma-2b-it.json \
+#    --prompt_type 1 \
+#    --inference_type chat \
+#   --save_dir_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff200 \
+#    --testset TRAIT_Dark
 
-python SAE/analysis.py \
+python SAE/analysis_dark.py \
     --save_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff200 \
     --prompt_type 1 \
     --model_name gemma-2b-it \
@@ -36,7 +36,7 @@ python SAE/sae_run.py \
     --save_dir_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff400 \
     --testset TRAIT_Dark
 
-python SAE/analysis.py \
+python SAE/analysis_dark.py \
     --save_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff400 \
     --prompt_type 1 \
     --model_name gemma-2b-it \
@@ -57,7 +57,28 @@ python SAE/sae_run.py \
     --save_dir_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff800 \
     --testset TRAIT_Dark
 
-python SAE/analysis.py \
+python SAE/analysis_dark.py \
+    --save_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff800 \
+    --prompt_type 1 \
+    --model_name gemma-2b-it \
+    --bg_features ../data/SAE/bg_features/coeff
+
+python SAE/sae_run.py \
+    --model_name gemma-2b-it \
+    --sae_name gemma-2b-it-res-jb \
+    --sae_id blocks.12.hook_resid_post \
+    --tokenizer_name google/gemma-2b-it \
+    --layer 12 \
+    --coeff 1000 \
+    --bg_type fixed \
+    --steer_mode \
+    --steer_file_path ../data/SAE/bg_features/coeff/gemma-2b-it.json \
+    --prompt_type 1 \
+    --inference_type chat \
+    --save_dir_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff800 \
+    --testset TRAIT_Dark
+
+python SAE/analysis_dark.py \
     --save_path ../data/SAE/steer_result/case_study/coeff_change/gemma-2b-it/gemma-2b-it_coeff800 \
     --prompt_type 1 \
     --model_name gemma-2b-it \
